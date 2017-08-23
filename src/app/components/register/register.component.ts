64bbc6c3ace3f970892a7d6ac7dc4ce20c02e3e5
DanielKell <danielkell.ubc@gmail.com>
2017-08-23 12:08:55 -0700

    Set up form control for the Register Page

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {JobService } from '../../services/job'; //Grab list of jobs to autopopulate options when registering a new colonist
-import {ColonistService} from '../../services/colonist';
+import { JobService } from '../../services/job'; //Grab list of jobs to autopopulate options when registering a new colonist
+import { ColonistService } from '../../services/colonist';
+import {FormControl, FormGroup} from '@angular/forms';
+import {Job} from '../../models/job';
 
 @Component({
   selector: 'app-register',
@@ -8,22 +10,40 @@ import {ColonistService} from '../../services/colonist';
   styles: [],
   providers: [JobService, ColonistService]
 })
+//The ngClass changes class when there are more than 10 items in the list
+
+
 export class RegisterComponent implements OnInit {
+
+
+  jobs: Job[]; //Created so can be used below
+
+  registerForm = new FormGroup({ //This is where we setup the form controls
+    name: new FormControl(''),
+    age: new FormControl(''),
+    job_id: new FormControl('')
+  });
+
 
   constructor(private jobService: JobService, private colonistService: ColonistService) { }
 
   async ngOnInit() {
+
     const jobs = await this.jobService.getJobs();
     console.log(jobs);
 
-        const data = { //This is temp for testing
-        name: 'Hello there',
-        age: '50',
-        job_id: '2'
-      }
+      this.jobs = jobs;
 
-    const newColonist = await this.colonistService.registerColonist(data);
-    console.log(newColonist);
+
+
+    // const data = { //This is temp for testing
+    //   name: 'Hello there',
+    //   age: '50',
+    //   job_id: '2'
+    // }
+    // const newColonist = await this.colonistService.registerColonist(data);
+    // console.log(newColonist);
+
   }
 
   //   async ngOnInit2() {
